7ce0d265f2983d2f2775f06d7d9e64a3d29c7a72
Fredrick <fredmara2016@gmail.com>
2022-09-08 13:42:14 -0700

    size six in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - print the string in the put function
+*this program prints "Programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizzeof(i));
+	printf("Size of a long int: %id byte(s)\n", sizeof(li));
+	Printf("Size of a long long int: %ld byte(s)\n" sizeof(lli));
+	Printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
