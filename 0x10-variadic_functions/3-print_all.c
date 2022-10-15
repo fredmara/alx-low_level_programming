006f63430a53a18db6736b709dd36e2d8ffc2838
Fredrick <fredmara2016@gmail.com>
2022-10-15 00:58:40 -0700

    correction

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,7 +8,7 @@
  *
  * Return: void
  */
-void print_c(va_list c
+void print_c(va_list c)
 {
 	printf("%c", va_arg(c, int));
 }
@@ -69,14 +69,13 @@ void print_all(const char * const format, ...)
 	va_list valist;
 	char *separator = "";
 
-char *separator = "";
-
+	va_start(valist, format);
 	i = 0;
 	while (format && format[i])
 	{
 		j = 0;
-		j = 0;
-
+		while (p[j].t != NULL)
+		{
 			if (*(p[j].t) == format[i])
 			{
 				printf("%s", separator);
